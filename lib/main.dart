feffce13ddb264fc8bcff1c6d08d94778da75711
Danny Castrillon Victoria <danny.castrillon1992@gmail.com>
2023-06-15 20:32:48 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const Pokedex());
+}
+
+class Pokedex extends StatelessWidget {
+  const Pokedex({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pokedex',
+    );
+  }
+}
